8fece78021ac28ab2817231def6eed095d310053
SergeyNE-edx-py <60068623+SergeyNE-edx-py@users.noreply.github.com>
2020-05-04 17:52:06 +0300

    Add files via upload

---
+++ js/checkbox-storage.js
@@ -0,0 +1,35 @@
+// Checkboxes - сохранение в localStorage, восстановление из localStorage, очистка localStorage
+let savedChecks = localStorage.getItem('checks').split(",").map(e => e == 'false' ? false : true)
+let checkBoxes = document.getElementsByClassName("form-check-input");
+let saveChecksButton = document.getElementById("SaveChecks");
+let clearStorageButton = document.getElementById("clearStorage");
+
+if (savedChecks) {
+    [...checkBoxes].map( (ch, i) => {
+        ch.checked = savedChecks[i];
+
+        ch.disabled = true;
+    })
+
+    saveChecksButton.disabled = true;           
+}
+
+saveChecksButton.addEventListener('click', () => {
+    let checks = [...checkBoxes].map(ch => ch.checked);
+
+    localStorage.setItem('checks', checks);
+
+    [...checkBoxes].map(ch => ch.disabled = true);
+    saveChecksButton.disabled = true;
+
+    console.log('Save Checks:', checks, localStorage.getItem('checks'));
+})
+
+clearStorageButton.addEventListener('click', () => {
+    localStorage.removeItem('checks');
+
+    [...checkBoxes].map(ch => ch.disabled = false);
+    saveChecksButton.disabled = false;
+
+    console.log('Clear localStorage:', localStorage.getItem('checks'));
+})
---
+++ js/input-cookie.js
@@ -0,0 +1,68 @@
+// Input1 - сохранение в cookie, восстановление из cookie, очистка cookie
+let savedInput = getCookie("Input");
+let inputField = document.getElementById("Input");
+let inputHelp = document.getElementById("InputHelp");
+let saveInputButton = document.getElementById("SaveInput");
+let clearCookieButton = document.getElementById("clearCookie");
+
+if (savedInput) {
+    inputField.value = document.getElementById("SaveInput");
+
+    inputField.disabled = true;
+    saveInputButton.disabled = true;
+
+    inputHelp.innerHTML = "Указанный город был ранее сохранен в cookie"
+
+    console.log("savedInput", inputField.value, inputField.disabled, saveInputButton.disabled)           
+}
+
+saveInputButton.addEventListener('click', () => {
+    setCookie("Input", inputField.value, {'max-age': 60})
+
+    inputField.disabled = true;
+    saveInputButton.disabled = true;
+
+    inputHelp.innerHTML = "Указанный город будет храниться в cookie <strong>в течение 1 мин</strong>"           
+
+    console.log('Save Input:', getCookie("Input"));
+})
+
+clearCookieButton.addEventListener('click', () => {
+    deleteCookie("Input");
+
+    inputField.disabled = false;
+    saveInputButton.disabled = false;
+
+    inputHelp.innerHTML = "По нажатию кнопки  <strong>Сохранить</strong>, указанный город сохранится в cookie"           
+
+    console.log('Clear Cookie:', getCookie("Input"));
+})
+
+// возвращает куки с указанным name, или undefined, если ничего не найдено      
+'use strict';
+function getCookie(name) {
+    let matches = document.cookie.match(new RegExp(
+        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+function setCookie(name, value, options = {}) {
+    let updatedCookie = name + "=" + value;
+
+    for (let optionKey in options) {
+        updatedCookie += "; " + optionKey;
+        let optionValue = options[optionKey];
+        if (optionValue !== true) {
+            updatedCookie += "=" + optionValue;
+        }
+    }
+
+    document.cookie = updatedCookie;
+}
+
+function deleteCookie(name) {
+    setCookie(name, "", {
+        'max-age': -1
+    })
+}      
