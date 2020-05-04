4fa8d86e51cf822c664b5353af0933dd53085238
SergeyNE-edx-py <60068623+SergeyNE-edx-py@users.noreply.github.com>
2020-05-04 18:51:47 +0300

    Add files via upload

---
+++ js/checkbox-storage.js
@@ -0,0 +1,37 @@
+// Checkboxes - сохранение в localStorage, восстановление из localStorage, очистка localStorage
+let savedChecks = localStorage.getItem('checks');
+let checkBoxes = document.getElementsByClassName("form-check-input");
+let saveChecksButton = document.getElementById("SaveChecks");
+let clearStorageButton = document.getElementById("clearStorage");
+
+if (savedChecks) {
+    savedChecks = savedChecks.split(",");
+    savedChecks = savedChecks.map(e => e == 'false' ? false : true);
+    [...checkBoxes].map( (ch, i) => {
+        ch.checked = savedChecks[i];
+
+        ch.disabled = true;
+    })
+
+    saveChecksButton.disabled = true;           
+}
+
+saveChecksButton.addEventListener('click', () => {
+    let checks = [...checkBoxes].map(ch => ch.checked);
+
+    localStorage.setItem('checks', checks);
+
+    [...checkBoxes].map(ch => ch.disabled = true);
+    saveChecksButton.disabled = true;
+
+    console.log('Save Checks:', checks, localStorage.getItem('checks'));
+})
+
+clearStorageButton.addEventListener('click', () => {
+    localStorage.removeItem('checks');
+
+    [...checkBoxes].map(ch => ch.disabled = false);
+    saveChecksButton.disabled = false;
+
+    console.log('Clear localStorage:', localStorage.getItem('checks'));
+})
